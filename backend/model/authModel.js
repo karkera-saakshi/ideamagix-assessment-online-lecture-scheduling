bfdfbc477b765d08ac8e165c29fdc1af31a73aad
karkera-saakshi <saakshikarkera81@gmail.com>
2026-08-05 12:58:21 +0530

    Changes in Login Module

--- backend/model/authModel.js
+++ backend/model/authModel.js
@@ -19,17 +19,26 @@ let createAccount = (obj, res) =>{
     .finally (()=>client.close())
 }
 
-let loginAccount = (obj, res) =>{
+let loginAccount = (obj, res) => {
     let client = new MongoClient(url);
-    client.connect();
-    let db = client.db("lecture-scheduling");
-    let coll = db.collection("users");
-    coll.findOne({ email: obj.email, password: obj.password })
-    .then((result)=> res.send(result))
-    .catch((err)=>res.send(err))
-    .finally (()=>client.close())
-}
-
+    client.connect()
+        .then(() => {
+            let db = client.db("lecture-scheduling");
+            let coll = db.collection("users");
+            return coll.findOne({ email: obj.email, password: obj.password });
+        })
+        .then((result) => {
+            if (!result) {
+                return res.status(401).send({ message: "Invalid email or password" });
+            }
+            return res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.error("Login error:", err);
+            return res.status(500).send({ message: "Server error during login", error: err.message });
+        })
+        .finally(() => client.close());
+};
 let getInstructors = (res) => {
     let client = new MongoClient(url);
     client.connect();
